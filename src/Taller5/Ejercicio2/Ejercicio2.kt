a65918c6a5e1d9c095aa760298a87fb8c423496c
Hunkillerx <jdiazseg9611@universidadean.edu.co>
2018-02-15 19:09:21 -0500

    Ejercicio 1,2,4 Falta documentacion

--- src/Taller5/Ejercicio2/Ejercicio2.kt
+++ src/Taller5/Ejercicio2/Ejercicio2.kt
@@ -1,16 +1,33 @@
 package Taller5.Ejercicio2
 
-class viajeCamion{
+import kotlin.math.roundToInt
+
+class viajeCamion {
     private var numKilosCapacidad = 0.0
     private var galonKm = 0.0
 
-    constructor(numKilosCapacidad:Double,galonKm:Double){
+    constructor(numKilosCapacidad: Double, galonKm: Double) {
         this.numKilosCapacidad = numKilosCapacidad
         this.galonKm = galonKm
     }
 
-    fun darCapacidad():Double{
+    fun darCapacidad(): Double {
         return numKilosCapacidad
+    }
+
+    fun darGalonKm(): Double {
+        return galonKm
+    }
+
+
+    fun numViajesGalones(kmsViaje: Double, cantidadCarga: Double): Pair<Int, Double> {
+        var numViajes = cantidadCarga / numKilosCapacidad
+        if (cantidadCarga % numKilosCapacidad != 0.0) {
+            numViajes++
+        }
+        var galonesPorViaje = ((numViajes * kmsViaje) * 2) * galonKm
+
+        return numViajes.roundToInt() to galonesPorViaje
     }
 
 
--- src/Taller5/Ejercicio2/viajeCamionTest.kt
+++ src/Taller5/Ejercicio2/viajeCamionTest.kt
@@ -8,6 +8,15 @@ internal class viajeCamionTest{
     @Test
     fun prueba1(){
 
-        val c = viajeCamion(2000,)
+        val c = viajeCamion(2000.0,6.0)
+
+        assertEquals(2000.0,c.darCapacidad())
+
+        assertEquals(6.0,c.darGalonKm())
+
+        val (a,b) = c.numViajesGalones(100.4,20000.0)
+        assertEquals(10,a)
+        assertEquals(12048.0,b)
+
     }
 }
