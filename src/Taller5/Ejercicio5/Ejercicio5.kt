843a3202c0aff6b3da11c64e85fa99e6e7e13cc8
Hunkillerx <jdiazseg9611@universidadean.edu.co>
2018-02-17 07:47:33 -0500

    Ejercicio 1,2,3,4,5 En el 5 falta Test   Falta documentacion

--- src/Taller5/Ejercicio5/Ejercicio5.kt
+++ src/Taller5/Ejercicio5/Ejercicio5.kt
@@ -1,9 +1,27 @@
 package Taller5.Ejercicio5
 
 class Avion{
-    private var pasajerosClase1 = 0
-    private var pasajerosClase2 = 0
-    private var pasajerosClase3 = 0
+    private var costoClase1 = 500_000
+    private var costoClase2 = 200_000
+    private var costoClase3 = 125_000
 
+    constructor(costoClase1: Int,costoClase2: Int,costoClase3: Int){
+        this.costoClase1 = costoClase1
+        this.costoClase2 = costoClase2
+        this.costoClase3 = costoClase3
+    }
 
+    fun recolectadoPorTiquetes(clase : Int,numPasajeros:Int):Int{
+        var rpt = 0
+        if (clase == 1){
+            rpt = costoClase1 * numPasajeros
+        }else if (clase == 2){
+            rpt = costoClase2 * numPasajeros
+        }else if(clase == 3){
+            rpt = costoClase3 * numPasajeros
+        }else{
+            println("La clase no existe")
+        }
+        return rpt
+    }
 }
